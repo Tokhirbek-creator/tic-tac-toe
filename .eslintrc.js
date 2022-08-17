74c260289dda73e3740b37fa3003ef0726c9e678
Tokhir <karamatov.tohirbek@gmail.com>
2022-08-17 16:55:29 +0300

    Board component

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,10 @@ module.exports = {
     'react/jsx-filename-extension': 0,
     'react/react-in-jsx-scope': 0,
     'react/button-has-type': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'react/prop-types': 0,
+    'no-unused-expressions': 0,
+    'no-bitwise': 0,
   },
 };
