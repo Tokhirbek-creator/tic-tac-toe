c067cc963433706a3201d54560c8ef90651780b6
Tokhir <karamatov.tohirbek@gmail.com>
2022-08-17 15:36:02 +0300

    Main component

--- src/components/Main/App.jsx
+++ src/components/Main/App.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import { useState } from 'react';
+import Board from '../Board/Board';
+import Users from '../Users/Users';
+import './app.css';
 
 function App() {
+  const [reset, setReset] = useState(false);
+  const [winner, setWinner] = useState('');
+
+  const resetBoard = () => {
+    setReset(true);
+  };
+
   return (
-    <div>App</div>
+    <div className="App">
+      <div className={`winner ${winner !== '' ? '' : 'shrink'}`}>
+        <div className="winner-text">{winner}</div>
+        <button onClick={() => resetBoard()}>
+          Reset Board
+        </button>
+      </div>
+      <Board
+        reset={reset}
+        setReset={setReset}
+        winner={winner}
+        setWinner={setWinner}
+      />
+      <Users />
+    </div>
   );
 }
 
